0dc1c207d9b682d8aa758da1d1ed85309e9affe9
varna <paul.varna@gmail.com>
2023-03-07 13:42:47 +0200

    remove nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,2 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  routeRules: {
-    '/api/**': { cors: true },
-  },
-  imports: {
-    dirs: ['models'],
-  },
-  nitro: {
-    imports: {
-      dirs: ['models'],
-    },
-  },
-});
+export default defineNuxtConfig({});
