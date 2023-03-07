83e02eeb11dc45061b12ddd0958a9d3accc1b7b4
varna <paul.varna@gmail.com>
2023-03-07 13:05:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-api-post-405
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-tocoqh)
---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="response">response: {{ response }}</div>
+  <div v-else-if="error">error: {{ error }}</div>
+  <div v-else>loading</div>
+</template>
+
+<script setup>
+const { data: response, error } = await useFetch(`/api/message`, {
+  method: 'POST',
+  body: {
+    message: 'test',
+  },
+});
+</script>
