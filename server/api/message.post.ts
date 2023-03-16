3da5d650464459901b3f0eb5037dafe335958af0
varna <paul.varna@gmail.com>
2023-03-16 19:25:00 +0200

    1 file modified

--- server/api/message.post.ts
+++ server/api/message.post.ts
@@ -1,8 +1,9 @@
 export default defineEventHandler((event) => {
-  // let body = await readBody(event);
-  // return `POST: ${body.message}`;
-  console.log(event.req.method);
-  return readBody(event)
-    .then((body) => body)
-    .catch(console.log);
+  // // let body = await readBody(event);
+  // // return `POST: ${body.message}`;
+  // console.log(event.req.method);
+  // return readBody(event)
+  //   .then((body) => body)
+  //   .catch(console.log);
+  return { a: 1 };
 });
