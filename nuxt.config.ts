fc33694b6bd330a84caec66b0813050e8fcd774a
varna <paul.varna@gmail.com>
2023-03-16 19:37:56 +0200

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   routeRules: {
-    '/api/**': { cors: true },
+    '/api/**': { cors: true, headers: { 'access-control-allow-methods': '*' } },
   },
 });
